7cef6e333e4cdabc15579e2e327029bb0350bd53
robertdcarlisle <robert.d.carlisle@gmail.com>
2026-02-02 16:13:49 -0700

    Cleaned AASA file.

--- src/cleaning.py
+++ src/cleaning.py
@@ -25,3 +25,35 @@ def clean_benchmarks(filename):
     df["student_id"] = df["student_id"].astype(str)
 
     return df
+
+bm1 = clean_benchmarks("2025-2026 BM1 Math.xlsx")
+bm1 = bm1.rename(columns={"bm_score": "bm1_score"})
+
+#Pretest Data preparation. 
+# I can reuse the same function as bm1 since the structure is the same.
+pretest = clean_benchmarks("2025-2026 Pretest Math.xlsx")
+pretest = pretest.rename(columns={"bm_score": "pretest_score"})
+
+#AASA Data preparation.
+def clean_AASA(filename):
+    
+    path = DATA_RAW / filename
+    df = pd.read_csv(path, delimiter="\t")
+
+    # filter to only Math tests
+    df = df[df['Test Code'].str.contains('AZAM', na=False)]
+
+    # rename important columns
+    df = df.rename(columns={
+        "SSID": "state_student_id",
+        "Total Scale Score": "ly_math_AASA_score"
+    })
+
+     # keep only what you need
+    df = df[["state_student_id", "ly_math_AASA_score"]]
+
+    # standardize types
+    df["state_student_id"] = df["state_student_id"].astype(str)
+    return df
+  
+aasa = clean_AASA("AZ_AASA_District_0004245_Spring_2025_Student_Data_File.txt") 
